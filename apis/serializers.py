85c27adbae0d75e1168233c2852e44c8b30d8531
Ali Hashempour <ali.hashempour@gmail.com>
2023-07-27 20:26:30 +0330

    feat :  filter and validate data

--- apis/serializers.py
+++ apis/serializers.py
@@ -9,6 +9,16 @@ class ArticleSerializer(serializers.ModelSerializer):
         model = Article
         exclude = ('id', 'created_at',)
 
+    def validate(self, data):
+        banned_words = ['LGBTQ', 'gay', 'pride']
+        body = data['body']
+        for word in banned_words:
+            if word in body:
+                raise serializers.ValidationError(
+                    "we dont support gay rights here :)"
+                )
+        return data
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
--- apis/views.py
+++ apis/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from apis.serializers import ArticleSerializer, UserSerializer
@@ -10,6 +11,8 @@ class ArticleListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Article.objects.filter(show=True)
     serializer_class = ArticleSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ['author__username', 'show']
 
 
 class ArticleDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -22,6 +25,8 @@ class UserListView(generics.ListCreateAPIView):
     permission_classes = (IsAdminUser,)
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ['username', 'email', 'is_staff', 'is_active']
 
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
