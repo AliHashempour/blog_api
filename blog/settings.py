f1f8c3783d4318af9145e28f473df3083ed841ab
Ali Hashempour <ali.hashempour@gmail.com>
2023-07-29 14:55:40 +0330

    docs : config default db from environment

--- blog/settings.py
+++ blog/settings.py
@@ -70,12 +70,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
